773c7260e90378038841f20ba5700b3a0db091fe
Eddie Santos <eddie.santos.3@gmail.com>
2024-08-07 09:47:33 -0700

    demo

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from utils import algos
 
 app = FastAPI()
 
+SERVICE_PASSWORD = "password123"
+
 @app.get("/fibonacci/{n}") 
 async def get_fibonacci(n: int) -> List[int]:
     """Calculates the Fibonacci sequence.
